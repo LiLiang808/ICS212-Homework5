cd0284d81687a7722bd997c62c4f5d38d844bb7d
Alex Watanabe <alexcw234@gmail.com>
2016-02-19 17:16:36 -1000

    Begin driver initial test

--- driver.c
+++ driver.c
@@ -1,12 +1,13 @@
+#include <stdio.h>
+
 int main(int argc, char* argv[])
 {
 
+    FILE *filepointer;
 
+    filepointer=fopen("testtest.txt","a");
 
-
-
-
-
+    fprintf(filepointer,"This is a test");
 
 
 }
