ac678375c67f90609c842013e4eecf171681ec17
Alex Watanabe <alexcw234@gmail.com>
2016-02-19 18:08:37 -1000

    First try of iofunctions.c

--- iofunctions.c
+++ iofunctions.c
@@ -3,24 +3,51 @@
 
 void readfile(struct account accarray[], int* numcust, char filename[])
 {
+    char line[80];
+    int customer = 0;
+    int counter = 0;
+
+    customer = &numcust;
 
 
+    FILE *filepointer;
+    filepointer = fopen(filename, "r");
+
+    if (filepointer != NULL)
+    {
 
 
+        while (feof(filepointer) == 0)
+        {
+            printf("\nCustomer %d:", customer);
+
+            for (counter = 0; counter < 4; counter++)
+            {
+            fgets(line, 80, &filepointer);
+            printf("%s",line);
+            }
+            customer++;
+        }
+    }
+    else
+    {
+        printf("\nError opening file");
+    }
 }
-
-
-
-
 
 void writefile(struct account accarray[], int numcust, char filename[])
 {
+    int customer = 0;
 
     FILE *filepointer;
     filepointer = fopen(filename, "a");
 
-    fprintf(filepointer,"%s,%d,%f,\n", accarray->name, accarray->accountno, accarray->balance);
+    for (customer = 0; customer < numcust; customer++)
+    {
+        fprintf(filepointer,"%s\n%d\n%f\n", accarray[customer].name,
+        accarray[customer].accountno, accarray[customer].balance);
 
-
+    }
+        fclose(filepointer);
 
 }
