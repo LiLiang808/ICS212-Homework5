e29dbdf7922483a0011e77e9bc6bd675a69143ab
Alex Watanabe <alexcw234@gmail.com>
2016-02-21 16:00:33 -1000

    Comments for headers, edit prototypes

--- account.h
+++ account.h
@@ -1,3 +1,20 @@
+/*****************************************************************
+//
+// NAME: Alex Watanabe
+//
+// HOMEWORK: 5
+//
+// CLASS: ICS 212
+//
+// INSTRUCTOR: Ravi Narayan
+//
+// DATE: Feb 20 , 2016
+//
+// FILE: account.h
+//
+// DESCRIPTION: This file contains the struct account for hw5
+//
+//****************************************************************/
 struct account
 {
     char        name[25];
--- ioheader.h
+++ ioheader.h
@@ -1,4 +1,20 @@
+/*****************************************************************
+//
+// NAME: Alex Watanabe
+//
+// HOMEWORK: 5
+//
+// CLASS: ICS 212
+//
+// INSTRUCTOR: Ravi Narayan
+//
+// DATE: Feb 20 , 2016
+//
+// FILE: ioheader.h
+//
+// DESCRIPTION: This file contains the prototype functions for hw5
+//
+//****************************************************************/
 #include "account.h"
-void readfile(struct account accarray[], int* numcust, char filename[]);
-void writefile(struct account accarray[], int numcust, char filename[]);
-
+void readfile(struct account accarray[], int*, char []);
+void writefile(struct account accarray[], int, char []);
